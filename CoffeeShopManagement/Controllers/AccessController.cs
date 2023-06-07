4520ae0064d83d4445eae92ca099930aa0a57a65
Vibuyn <quan02vip@gmail.com>
2023-06-07 22:49:59 +0700

    first commit

---
+++ CoffeeShopManagement/Controllers/AccessController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopManagement.Models;
+
+namespace CoffeeShopManagement.Controllers
+{
+    public class AccessController : Controller
+    {
+        CoffeeShopManagementContext db = new CoffeeShopManagementContext();
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if(HttpContext.Session.GetString("UserName") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index","Home"); 
+            }
+        }
+        [HttpPost]
+        public IActionResult Login(NguoiDung  user)
+        {
+            if(HttpContext.Session.GetString("UserName")==null)
+            {
+                var u = db.NguoiDungs.Where(x => x.IdNguoiDung.Equals(user.IdNguoiDung) && x.MatKhau.Equals
+                (user.MatKhau)).FirstOrDefault();
+                if(u!=null)
+                {
+                    HttpContext.Session.SetString("UserName", u.IdNguoiDung.ToString());
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.Session.Remove("UserName");
+            return RedirectToAction("Login","Access");
+        }
+    }
+}
